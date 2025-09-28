b315560bb80aa8b0254a903635378d7873a6b91e
Nataliia Liubynets <natalialiubynets@gmail.com>
2025-09-28 22:57:31 +0200

    feat(theme): save chosen theme in localStorage

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import './style.css';
 import { createStars } from './createStars';
-import {toggleMode} from './utils';
-
+import { toggleMode } from './utils';
 
 const toggleButton = document.querySelector('#theme');
 
+const theme = localStorage.getItem('theme');
 createStars();
 toggleButton?.addEventListener('click', toggleMode);
+
+if (theme) {
+	document.documentElement.classList.add('dark');
+}
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,9 @@
 export function toggleMode() {
+	if(!localStorage.getItem('theme')){
+		localStorage.setItem('theme', 'dark');
+	} else {
+		localStorage.removeItem('theme');
+	};
 	document.documentElement.classList.toggle('dark');
+	
 }
